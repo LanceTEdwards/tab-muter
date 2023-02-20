0c5da5c03e102ff92952337bb442739b22eee4da
LanceTEdwards <lance.edwards@education.govt.nz>
2023-02-20 16:42:03 +0800

    First commit.

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,28 @@
 let checkbox = document.querySelector('#checkbox');
 
-    checkbox.addEventListener('change', () => {
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.storage.local.get('enabled', function (result) {
+        if (result.enabled != null) {
+            checkbox.checked = result.enabled;
+        }
+        else if(result.enabled == null) {
+            checkbox.checked = !result.enabled;
+        }
+    });
+    checkbox.addEventListener('change', function () {
+        chrome.storage.local.set({ 'enabled': checkbox.checked}, function () {
+        });
+
         chrome.tabs.query({url: []}, function (tabs) {
             for (let i = 0; i < tabs.length; i++) {
                 let mutedInfo = tabs[i].mutedInfo.muted;
-                if (!mutedInfo && checkbox.checked === true) {
+                if (!mutedInfo && checkbox.checked == true) {
                     chrome.tabs.update(tabs[i].id, {"muted": true});
                 }
-                else if(mutedInfo && checkbox.checked === false){
+                else if(mutedInfo && checkbox.checked == false) {
                     chrome.tabs.update(tabs[i].id, {"muted": false});
                 }
             }
         });
     });
+});
--- scripts/background.js
+++ scripts/background.js
@@ -1,23 +1,4 @@
-function saveCheckBoxState(checkbox) {
-    chrome.storage.local.set({'checkbox': checkbox}, function() {
-        console.log("State saved");
-    });
-}
-
-function saveMutedState(mutedState) {
-    chrome.storage.local.set({'muted': mutedState}, function() {
-        console.log("State saved");
-    });
-}
-
-function loadCheckBoxState(checkbox) {
-    chrome.storage.local.get({'checkbox': checkbox}, function() {
-        console.log("State loaded");
-    });
-}
-
-function loadMutedState(mutedState) {
-    chrome.storage.local.get({'muted': mutedState}, function() {
-        console.log("State loaded");
-    });
-}
+chrome.storage.local.get('enabled', function(result) {
+    if (result.enabled) {
+    }
+});
