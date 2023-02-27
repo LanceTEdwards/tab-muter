5bd615cd2f6434c7f73704e531ef446abe9b7981
LanceTEdwards <lance.edwards@education.govt.nz>
2023-02-27 16:21:46 +1300

    cleaned up some of the commented out code and added some comments

--- popup/popup.js
+++ popup/popup.js
@@ -1,28 +1,27 @@
-let checkbox = document.querySelector('#checkbox');
+let checkbox = document.querySelector("#checkbox");
 
-document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.local.get('enabled', function (result) {
-        if (result.enabled != null) {
-            checkbox.checked = result.enabled;
-        }
-        else if(result.enabled == null) {
-            checkbox.checked = !result.enabled;
-        }
-    });
-    checkbox.addEventListener('change', function () {
-        chrome.storage.local.set({ 'enabled': checkbox.checked}, function () {
-        });
+//created a variable to access the checkbox to execute actions when it has been checked/unchecked
 
-        chrome.tabs.query({url: []}, function (tabs) {
-            for (let i = 0; i < tabs.length; i++) {
-                let mutedInfo = tabs[i].mutedInfo.muted;
-                if (!mutedInfo && checkbox.checked == true) {
-                    chrome.tabs.update(tabs[i].id, {"muted": true});
-                }
-                else if(mutedInfo && checkbox.checked == false) {
-                    chrome.tabs.update(tabs[i].id, {"muted": false});
-                }
-            }
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.local.get("enabled", function (result) {
+    if (result.enabled != null) {
+      checkbox.checked = result.enabled;
+    }
+    //when the window loads, check the storage to check whether there was stored input
+    //Afterword: tried to store the muted state but was unsure on how to do it
+  });
+  checkbox.addEventListener("change", function () {
+    chrome.storage.local.set({ enabled: checkbox.checked }, function () {});
+
+    chrome.tabs.query({ url: [] }, function (tabs) {
+      for (let i = 0; i < tabs.length; i++) {
+        let mutedInfo = tabs[i].mutedInfo.muted;
+        if (!mutedInfo && checkbox.checked == true) {
+          chrome.tabs.update(tabs[i].id, { muted: true });
+        } else if (mutedInfo && checkbox.checked == false) {
+          chrome.tabs.update(tabs[i].id, { muted: false });
+        }
+      }
     });
+  });
 });
--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,6 @@
-chrome.storage.local.get('enabled', function(result) {
-    if (result.enabled) {
-    }
+chrome.storage.local.get("enabled", function (result) {
+  if (result.enabled) {
+  }
 });
+
+//this accesses the chrome's local storage and gets wherever the 'enabled' function is set
